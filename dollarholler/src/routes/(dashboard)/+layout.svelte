5fd040d827c680341251ffe87db4cea5fac90cbb
Maria Paula Vivi <mariapaulaviviz@gmail.com>
2023-05-04 17:40:05 -0500

    finish lesson 11

---
+++ dollarholler/src/routes/(dashboard)/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import NavBar from "$lib/components/NavBar.svelte";
+</script>
+
+<div class="grid grid-cols-12 gap-x-16 bg-whisper min-h-screen">
+    <NavBar />
+    <main class="col-span-8 pt-10">
+        <slot />
+    </main>
+</div>
